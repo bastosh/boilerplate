b5e0f5204866d0def00c50e9338559f83bca467a
Sébastien Pereda <sebastien.pereda@gmail.com>
2018-06-14 16:52:02 +0200

    Compression des images en production

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var ghPages = require('gulp-gh-pages');
+var imagemin = require('gulp-imagemin');
 
 
 gulp.task('css', function() {
@@ -8,6 +9,13 @@ gulp.task('css', function() {
         .pipe(cleanCSS())
         .pipe(gulp.dest('dist/css'));
 });
+
+gulp.task('img', function() {
+    gulp.src('src/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+    }
+);
 
 gulp.task('copy', function() {
    return gulp.src('src/index.html')
@@ -18,5 +26,7 @@ gulp.task('deploy', function() {
     return gulp.src('./dist/**/*')
         .pipe(ghPages());
 });
+
+gulp.task('prod', ['css', 'copy', 'img']);
 
 gulp.task('default', ['css', 'copy']);
